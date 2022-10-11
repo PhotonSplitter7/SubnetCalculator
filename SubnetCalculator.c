18015a29361c6482e212ecc1d71332071ccc83c8
PhotonSplitter7 <70785987+PhotonSplitter7@users.noreply.github.com>
2022-10-11 09:25:41 -0400

    Dan
    update

--- SubnetCalculator.c
+++ SubnetCalculator.c
@@ -4,15 +4,19 @@
 
 int main(){
 //4 octets to hold binary IP address thats converted
-int octet1[8];
-int octet2[8];
-int octet3[8];
-int octet4[8];
+int BinAddr [4][8];
 
-int input =0;
+int inputAddress =0;
+int inputMask = 0;
 
 printf("Enter IP address:\n\n");
-scanf("%d", &input);//get user address
+scanf("%d", &inputAddress);//get user address
+printf("Enter subnet mask:\n\n");
+scanf("%d", &inputMask);
+
+
+
+
 
 
 
