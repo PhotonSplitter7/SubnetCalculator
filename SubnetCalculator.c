4eacb3eabfd336f040205d1efb8ff87920c27da5
PhotonSplitter7 <70785987+PhotonSplitter7@users.noreply.github.com>
2022-10-11 16:13:39 -0400

    dan g
    working basic

--- SubnetCalculator.c
+++ SubnetCalculator.c
@@ -4,15 +4,41 @@
 
 int main(){
 //4 octets to hold binary IP address thats converted
+char addrString[16];
+char maskString[16];
+int inputAddress[4];//holds 4 octets need to seperate by decimals
+int inputMask[4];
 int BinAddr [4][8];
+int BinMask [4][8];
 
-int inputAddress =0;
-int inputMask = 0;
+int cdrNum = 0;
+int numNets = 0;
 
 printf("Enter IP address:\n\n");
-scanf("%d", &inputAddress);//get user address
+scanf("%s", addrString);//get user address
 printf("Enter subnet mask:\n\n");
-scanf("%d", &inputMask);
+scanf("%s", maskString);// get subnet mask
+
+//convert string to int
+sscanf(addrString, "%d.%d.%d.%d", &inputAddress[0], &inputAddress[1],&inputAddress[2],&inputAddress[3]);
+sscanf(maskString, "%d.%d.%d.%d", &inputMask[0], &inputMask[1],&inputMask[2],&inputMask[3]);
+
+//fill binary address array with 1's or 0's
+for(int i = 0; i<4; i++){
+for(int j = 0; j < 8; j++){
+BinAddr[i][j]= inputAddress[i]%2;
+inputAddress[i] = inputAddress[i]/2;
+}
+}
+
+for(int j = 0; j< 4;j++){
+for(int i = 0; i< 8; i++){
+    printf("%d", BinAddr[j][i]);
+
+}
+}
+
+
 
 
 
