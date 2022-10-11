a486de5f50edf5d097c96ee36693cb5d8e41a5a7
PhotonSplitter7 <70785987+PhotonSplitter7@users.noreply.github.com>
2022-10-11 09:21:22 -0400

    Dan G
    first code

--- SubnetCalculator.c
+++ SubnetCalculator.c
@@ -3,7 +3,20 @@
 #include <string.h>
 
 int main(){
+//4 octets to hold binary IP address thats converted
+int octet1[8];
+int octet2[8];
+int octet3[8];
+int octet4[8];
+
+int input =0;
+
+printf("Enter IP address:\n\n");
+scanf("%d", &input);//get user address
+
 
 
 return 0;
 }
+
+
