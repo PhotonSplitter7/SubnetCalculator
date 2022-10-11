a9bf4ef0994ae4d329de1825fc7531219ede499b
PhotonSplitter7 <70785987+PhotonSplitter7@users.noreply.github.com>
2022-10-11 18:25:42 -0400

    dan g
    working net size, cidr num, and binary

--- SubnetCalculator.c
+++ SubnetCalculator.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 int main(){
 //4 octets to hold binary IP address thats converted
 char addrString[16];
 char maskString[16];
-int inputAddress[4];//holds 4 octets need to seperate by decimals
-int inputMask[4];
+
+int ipAddress[4];
+int subMask[4];
+int firstAddr[4];
+int lastAddr[4];
+int gateway[4];
+int broadcast[4];
+int cidrNum = 0;
+int sizeOfNetwork = 0;
+
 int BinAddr [4][8];
 int BinMask [4][8];
 
-int cdrNum = 0;
-int numNets = 0;
+
+
 
 printf("Enter IP address:\n\n");
 scanf("%s", addrString);//get user address
@@ -20,33 +29,59 @@ printf("Enter subnet mask:\n\n");
 scanf("%s", maskString);// get subnet mask
 
 //convert string to int
-sscanf(addrString, "%d.%d.%d.%d", &inputAddress[0], &inputAddress[1],&inputAddress[2],&inputAddress[3]);
-sscanf(maskString, "%d.%d.%d.%d", &inputMask[0], &inputMask[1],&inputMask[2],&inputMask[3]);
+sscanf(addrString, "%d.%d.%d.%d", &ipAddress[0], &ipAddress[1],&ipAddress[2],&ipAddress[3]);
+sscanf(maskString, "%d.%d.%d.%d", &subMask[0], &subMask[1],&subMask[2],&subMask[3]);
 
-//fill binary address array with 1's or 0's
+
+
+//compute binary IP address
 for(int i = 0; i<4; i++){
 for(int j = 0; j < 8; j++){
-BinAddr[i][j]= inputAddress[i]%2;
-inputAddress[i] = inputAddress[i]/2;
+BinAddr[i][j]= ipAddress[i]%2;
+ipAddress[i] = ipAddress[i]/2;
 }
 }
 
+//compute binary subnet mask
+for(int i = 0; i<4; i++){
+for(int j = 0; j < 8; j++){
+BinMask[i][j]= subMask[i]%2;
+subMask[i] = subMask[i]/2;
+}
+}
+
+//************************compute network addresses*****************
+
+
+
+//calculate cidr num
+ for(int r = 0; r< 4;r++){
+for(int c = 0; c< 8; c++){
+    if(BinMask[r][c] == 1){
+        cidrNum++;
+    }
+}}
+
+printf("\n\n CDR num is: %d\n\n\n", cidrNum);
+
+//calculate size of network
+sizeOfNetwork = (pow(2, (32 - cidrNum))-2);
+printf("\nsize of network is %d\n", sizeOfNetwork);
+
+//
+
+
+
+//print binary string for mask
 for(int j = 0; j< 4;j++){
 for(int i = 0; i< 8; i++){
-    printf("%d", BinAddr[j][i]);
-
+    printf("%d", BinMask[j][i]);
 }
 }
-
-
-
-
-
-
-
-
 
 return 0;
 }
+
+
 
 
